296b15e4b35c79e742ca999e00f31deec02cba2b
yossi shaul <76476648+yosef1sh@users.noreply.github.com>
2024-03-07 11:53:38 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,11 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
-// app.use(cors());
 
 app.use("/api/users", userRoutes);
-
 
 app.use(cors());
 
